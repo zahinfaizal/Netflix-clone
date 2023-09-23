47a747371ea9cdb62d7d13e51da9b7eaf4a891bd
haseeb360024 <muhammedhaseeb315@gmail.com>
2023-09-23 18:11:28 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Row from './components./Row';
+
+function App() {
+  return(
+    <div>
+      <h1>Netflix App</h1>
+      <Row/>
+    </div>
+  )
+}
+
+export default App;
